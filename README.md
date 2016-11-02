256a15f324af57f19ce87480e119684bb136e12d
Jeremy Sells <mrjeremysells@gmail.com>
2016-11-02 19:38:13 +1300

    Adding code to Github that I had as a zip on my website

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# WakeOnLanShutdown
-Wake On Lan Shutdown (WOLW) - Abandoned
+# WakeOnLanWebDesktopClient
+Wake On Lan (WOL) Web (WOLW) Desktop Client - Abandoned
+
+# WARNING
+Please note that this is an example only. There is no authentication before issuing a shutdown!
+
+# LICENSE
+Copyright (c) 2011 Jeremy Sells
+See the file LICENSE for copying permission.
+ 
+# ABOUT
+This application was complimentry to my WakeOnLanWebServer server application and allows computers to be shutdown from the web interface.
+Running this application, anyone can shutdown your pc remotely without a password
---
+++ src/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+    Copyright (c) 2011 Jeremy Sells
+    See the file LICENSE for copying permission.
+    Please read README.md before running or compiling as there is no security on this!
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+using System.Configuration;
+using System.Threading;
+
+namespace WOLW_PCclient
+{
+    class Program
+    {
+
+        /**
+         * Main Loop
+         */
+        static void Main(string[] args)
+        {
+             TcpListener tcpListener = new TcpListener(System.Net.IPAddress.Any, 500); ;
+             tcpListener.Start();
+
+             Boolean shutdownCommencing = false;
+             while (!shutdownCommencing)
+             {
+                Socket socketForClient = tcpListener.AcceptSocket();
+                if (socketForClient.Connected)
+                {
+                    Console.WriteLine("Client connected");
+                    NetworkStream networkStream = new NetworkStream(socketForClient);
+                    System.IO.StreamReader streamReader = new System.IO.StreamReader(networkStream);
+                    String theString = streamReader.ReadLine();
+                    if (theString == "Shutdown pc now please")
+                    {
+                        System.Diagnostics.Process.Start("shutdown", "-s -t 100");
+                    }
+
+                    Console.WriteLine(theString);
+                    streamReader.Close();
+                    networkStream.Close();
+                }
+                socketForClient.Close();
+                //Console.WriteLine("Trying Again...");
+            }
+        }
+    }
+}
